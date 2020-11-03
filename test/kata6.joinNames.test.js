575e464c935e4b7eecba7cfaaf1947541aec1f66
Dario Milani <runner855@icloud.com>
2020-11-03 16:45:48 +0100

    test folder changed

--- test/kata6.joinNames.test.js
+++
@@ -1,9 +0,0 @@
-const { 
-  joinNames 
-} = require("../src");
-
-describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {
-    expect(joinNames([{name: 'Bart'}, {name: 'Lisa'}, {name: 'Omer'}])).toBe('Bart, Lisa & Omer');
-  });
-});
