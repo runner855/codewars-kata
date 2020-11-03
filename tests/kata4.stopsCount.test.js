94038ca7ba9696228bf9089d7790acd8d8829941
Dario Milani <runner855@icloud.com>
2020-11-03 16:44:17 +0100

    updated tests folder

--- tests/kata4.stopsCount.test.js
+++ tests/kata4.stopsCount.test.js
@@ -3,7 +3,7 @@ const {
     } = require("../src");
 
 describe("stopsCount", () => {
-    test("return number of people who are still in the bus after the last bus station ", () => {
+    test("Each  array has two items, number of people get into bus  and number of people get off the bus ,return number of people who are still in the bus after the last bus station ", () => {
         expect(stopsCount([[10, 0], [3, 5], [5, 8]])).toEqual(5);
         expect(stopsCount([[3, 0],[9, 1],[4, 10],[12, 2],[6, 1],[7, 10]])).toEqual(17);
         expect(stopsCount([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]])).toEqual(21);
